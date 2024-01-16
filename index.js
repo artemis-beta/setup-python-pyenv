168b01b0a9fdeb69a751d520584b25668c5b24e6
Kristian Zarębski <krizar312@gmail.com>
2024-01-16 21:08:07 +0000

    Continue fixes of action

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
 // require the library, main export is a function
 const core = require('@actions/core');
+const fs = require('fs').promises;
+const path = require('path');
 const simpleGit = require('simple-git');
 const exeq = require('exeq');
 const repositoryUrl = "https://github.com/pyenv/pyenv.git";
-const destinationPath = "~/.pyenv";
+const destinationPath = path.resolve("pyenv");
 
 try {
-    simpleGit()
-        .clone(repositoryUrl, destinationPath)
-        .then(console.log("Cloning of PyEnv successful."));
-    const python_version = core.getInput('python-version');
-    const setup_poetry = core.getInput('setup-poetry');
-    exeq(
-        '~/.pyenv/bin/pyenv',
-        'install',
-        python_version
-    ).then(function() {
-        console.log(`Python ${python_version} was installed successfully`);
-    })
-
-    if (setup_poetry !== '') {
-        exeq(
-            `~/.pyenv/versions/${python_version}/bin/pip`,
-            'install',
-            'poetry'
-        ).then(function() {
-            console.log("Poetry setup successfully");
-        })
-    }
+simpleGit()
+    .clone(repositoryUrl, destinationPath)
+    .then(core.info("Cloning of PyEnv successful."));
 } catch(error) {
     core.setFailed(error.message);
+}
+
+fs.access(destinationPath, fs.constants.F_OK)
+    .then(() => {
+        core.debug(`Cloned pyenv to ${destinationPath}`);
+    })
+    .catch(() => {
+        core.setFailed("Cloning of pyenv failed.");
+    });
+
+const python_version = core.getInput('python-version');
+const setup_poetry = core.getInput('setup-poetry');
+
+exeq(
+    core.toPlatformPath(`${destinationPath}/bin/pyenv`),
+    'install',
+    python_version
+)
+.then(function() {
+    core.info(`Python ${python_version} was installed successfully`);
+})
+.catch(error => core.setFailed(error.message));
+
+
+if (setup_poetry !== '') {
+    exeq(
+       core.toPlatformPath(`${destinationPath}/versions/${python_version}/bin/pip`),
+        'install',
+        'poetry'
+    )
+    .then(function() {
+        core.info("Poetry setup successfully");
+    })
+    .catch(error => core.setFailed(error.message));
 }
