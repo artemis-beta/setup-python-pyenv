190d647dd03d31beb7b5dc0562bb4b5cdf4efb81
Kristian Zarębski <krizar312@gmail.com>
2024-01-16 23:11:46 +0000

    Tidy up and check Python available

--- index.js
+++ index.js
@@ -1,40 +1,77 @@
 // require the library, main export is a function
 const core = require('@actions/core');
 const fs = require('fs');
+const semver = require('semver');
 const path = require('path');
 const gitPullOrClone = require('git-pull-or-clone');
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
+const { stdout, exit } = require('process');
 const repositoryUrl = "https://github.com/pyenv/pyenv.git";
 const destinationPath = path.resolve("pyenv");
 const python_version = core.getInput('python-version');
 const setup_poetry = core.getInput('setup-poetry');
+
+const execOptions = {
+    encoding: 'utf8', // Set the encoding of the output
+};
+
+if (semver.valid(python_version) === null) {
+    core.setFailed(`⛔ Invalid semantic version '${python_version}'`);
+    exit(1);
+}
+
+core.startGroup("Setup PyEnv");
 
 gitPullOrClone(repositoryUrl, destinationPath, (error) => {
     if (error) core.setFailed(error.message);
 
     const pyenvBinary = path.join(destinationPath, "bin", "pyenv");
 
+    if (!fs.existsSync(pyenvBinary, fs.constants.F_OK)) {
+        core.setFailed(`❌ Cloning of pyenv failed: ${error}`);
+        exit(1);
+    }
+
+    core.debug(`🗂️  Cloned pyenv to ${destinationPath}`);
+
     try {
-        fs.existsSync(pyenvBinary, fs.constants.F_OK);
-        core.debug(`Cloned pyenv to ${destinationPath}`);
+        let std_out = execSync(`${pyenvBinary} install --list`, execOptions);
+        if (!std_out.includes(python_version)) {
+            core.setFailed(`❓ Cannot install Python '${python_version}', version not available.`);
+            exit(1);
+        }
+    } catch (error) {
+        core.setFailed(`❌ Failed to retrieve available python versions: ${error.message}`);
+        exit(1);
+    }
+
+    try {
+        execSync(`${pyenvBinary} install ${python_version}`, execOptions);
+    } catch (error) {
+        core.setFailed(`❌ Python install failed: ${error.message}`);
+        exit(1);
+    }
+
+    core.info(`🐍 Python ${python_version} was installed successfully`);
+    core.endGroup();
+
+    pythonBinDir =  core.toPlatformPath(`${destinationPath}/versions/${python_version}/bin`);
+
+    if (setup_poetry !== '') {
+        core.startGroup("Setup Poetry");
+        pythonPipExe = path.join(pythonBinDir, "pip");
         exec(
-            `${pyenvBinary} install ${python_version}`,
+            `${pythonPipExe} install poetry`,
             (error, _, __) => {
                 if (error) core.setFailed(error.message);
-                core.info(`Python ${python_version} was installed successfully`);
-                if (setup_poetry !== '') {
-                    pythonPipExe = core.toPlatformPath(`${destinationPath}/versions/${python_version}/bin/pip`);
-                    exec(
-                        `${pythonPipExe} install poetry`,
-                        (error, _, __) => {
-                            if (error) core.setFailed(error.message);
-                            core.info("Poetry setup successfully");
-                        }
-                    )
-                }
+                core.info("📖 Poetry setup completed successfully.");
+                core.addPath(pythonBinDir);
+                core.endGroup();
             }
         );
-    } catch(error) {
-        core.setFailed(`Cloning of pyenv failed: ${error}`);
-    }
+    };
+    core.startGroup("Updating environment");
+    core.info("🛫 Exporting environment variables");
+    core.endGroup();
+
 });
