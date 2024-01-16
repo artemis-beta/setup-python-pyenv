3ee058e66a36e33dda7d753442ca277e0da0a0af
Kristian Zarębski <krizar312@gmail.com>
2024-01-16 22:37:16 +0000

    USe git-pull-or-clone

--- index.js
+++ index.js
@@ -1,50 +1,40 @@
 // require the library, main export is a function
 const core = require('@actions/core');
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
-const simpleGit = require('simple-git');
-const exeq = require('exeq');
+const gitPullOrClone = require('git-pull-or-clone');
+const { exec } = require('child_process');
 const repositoryUrl = "https://github.com/pyenv/pyenv.git";
 const destinationPath = path.resolve("pyenv");
-
-try {
-simpleGit()
-    .clone(repositoryUrl, destinationPath)
-    .then(core.info("Cloning of PyEnv successful."));
-} catch(error) {
-    core.setFailed(error.message);
-}
-
-fs.access(destinationPath, fs.constants.F_OK)
-    .then(() => {
-        core.debug(`Cloned pyenv to ${destinationPath}`);
-    })
-    .catch(() => {
-        core.setFailed("Cloning of pyenv failed.");
-    });
-
 const python_version = core.getInput('python-version');
 const setup_poetry = core.getInput('setup-poetry');
 
-exeq(
-    core.toPlatformPath(`${destinationPath}/bin/pyenv`),
-    'install',
-    python_version
-)
-.then(function() {
-    core.info(`Python ${python_version} was installed successfully`);
-})
-.catch(error => core.setFailed(error.message));
+gitPullOrClone(repositoryUrl, destinationPath, (error) => {
+    if (error) core.setFailed(error.message);
 
+    const pyenvBinary = path.join(destinationPath, "bin", "pyenv");
 
-if (setup_poetry !== '') {
-    exeq(
-       core.toPlatformPath(`${destinationPath}/versions/${python_version}/bin/pip`),
-        'install',
-        'poetry'
-    )
-    .then(function() {
-        core.info("Poetry setup successfully");
-    })
-    .catch(error => core.setFailed(error.message));
-}
+    try {
+        fs.existsSync(pyenvBinary, fs.constants.F_OK);
+        core.debug(`Cloned pyenv to ${destinationPath}`);
+        exec(
+            `${pyenvBinary} install ${python_version}`,
+            (error, _, __) => {
+                if (error) core.setFailed(error.message);
+                core.info(`Python ${python_version} was installed successfully`);
+                if (setup_poetry !== '') {
+                    pythonPipExe = core.toPlatformPath(`${destinationPath}/versions/${python_version}/bin/pip`);
+                    exec(
+                        `${pythonPipExe} install poetry`,
+                        (error, _, __) => {
+                            if (error) core.setFailed(error.message);
+                            core.info("Poetry setup successfully");
+                        }
+                    )
+                }
+            }
+        );
+    } catch(error) {
+        core.setFailed(`Cloning of pyenv failed: ${error}`);
+    }
+});
